b4429719e889f4ecf42d06d05dc5e952aa1e149a
tls427wodnr <tlswodnr427@naver.com>
2025-03-10 09:37:06 +0900

    basic layout

--- CompositionalLayoutPractice/ViewController.swift
+++ CompositionalLayoutPractice/ViewController.swift
@@ -1,19 +1,150 @@
-//
-//  ViewController.swift
-//  CompositionalLayoutPractice
-//
-//  Created by tlswo on 3/7/25.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class CarouselCell: UICollectionViewCell {
+    static let reuseIdentifier = "CarouselCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .green
+        self.layer.cornerRadius = 8
     }
-
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
+class GridCell: UICollectionViewCell {
+    static let reuseIdentifier = "GridCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .blue
+        self.layer.cornerRadius = 8
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class TwoByTwoGridCell: UICollectionViewCell {
+    static let reuseIdentifier = "TwoByTwoGridCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .orange
+        self.layer.cornerRadius = 8
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class ViewController: UIViewController {
+    enum Section: Int, CaseIterable {
+        case carousel
+        case grid
+        case twoByTwoGrid
+    }
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollectionView()
+        configureDataSource()
+    }
+    
+    func configureCollectionView() {
+        let layout = createCompositionalLayout()
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        
+        collectionView.register(CarouselCell.self, forCellWithReuseIdentifier: CarouselCell.reuseIdentifier)
+        collectionView.register(GridCell.self, forCellWithReuseIdentifier: GridCell.reuseIdentifier)
+        collectionView.register(TwoByTwoGridCell.self, forCellWithReuseIdentifier: TwoByTwoGridCell.reuseIdentifier)
+    }
+    
+    func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, _ in
+            let section = Section.allCases[sectionIndex]
+            switch section {
+            case .carousel:
+                return self.createCarouselSection()
+            case .grid:
+                return self.createGridSection()
+            case .twoByTwoGrid:
+                return self.createTwoByTwoGridSection()
+            }
+        }
+    }
+    
+    func createCarouselSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 0, leading: 8, bottom: 0, trailing: 8)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.8))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 10
+        return section
+    }
+    
+    func createGridSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.25))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    func createTwoByTwoGridSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView, indexPath, item) -> UICollectionViewCell? in
+            let section = Section.allCases[indexPath.section]
+            switch section {
+            case .carousel:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCell.reuseIdentifier, for: indexPath)
+                return cell
+            case .grid:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GridCell.reuseIdentifier, for: indexPath)
+                return cell
+            case .twoByTwoGrid:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TwoByTwoGridCell.reuseIdentifier, for: indexPath)
+                return cell
+            }
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.carousel, .grid, .twoByTwoGrid])
+        snapshot.appendItems(Array(0..<5), toSection: .carousel)
+        snapshot.appendItems(Array(5..<21), toSection: .grid)
+        snapshot.appendItems(Array(21..<25), toSection: .twoByTwoGrid)
+        
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
